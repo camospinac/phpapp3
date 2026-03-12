9e0d78a7076c46875b4f5921de5c776b97a3a202
Camilo Ospina <camospinac@outlook.com>
2026-03-12 00:53:18 -0500

    fasfsadfa

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -316,7 +316,7 @@ const staticPaymentMethods = [
     { 
         id: 1, 
         name: 'Nequi', 
-        logo_path: 'https://cdn.prod.website-files.com/6317a229ebf7723658463b4b/663a6b0d43303ddf38035997_logo-nequi.svg' 
+        logo_path: 'https://brandemia.org/contenido/subidas/2023/10/nequi-nuevo-logotipo-1200x670.jpg' 
     },
     { 
         id: 2, 
